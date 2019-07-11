bda72cfa72b72f6ad50a395944b32a732f8d2c25
Edeilton Oliveira <edeiltonso@gmail.com>
2019-07-10 23:37:23 -0300

    Adicionado EnableCorsAttribute

--- BaseConverterAPI/App_Start/WebApiConfig.cs
+++ BaseConverterAPI/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace BaseConverterAPI
 {
@@ -9,6 +10,9 @@ namespace BaseConverterAPI
     {
         public static void Register(HttpConfiguration config)
         {
+            EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:3000, https://baseconverterapp.herokuapp.com, http://baseconverterapp.herokuapp.com", "*", "GET");
+            config.EnableCors(cors);
+
             config.MapHttpAttributeRoutes();
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
